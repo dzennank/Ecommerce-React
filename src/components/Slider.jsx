e7dce2049e6e819bb91fba7344176f0a01c865dd
dzennank <dzenankrlic@gmail.com>
2022-05-30 14:27:15 +0200

    ..

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import styled from 'styled-components';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
 const Container = styled.div` 
 display: flex;
 width: 100%;
 height: 100vh;
-background-color: coral;
 position: relative;
 `
 const Arrow = styled.div`
@@ -29,16 +29,47 @@ const Arrow = styled.div`
 
 `
 const Wrapper = styled.div`
-
+height: 100%;
+  display: flex;
 `
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  
+`;
+
 const Image = styled.img`
+height: 100%;
+
 
 `
 const ImgContainer = styled.div`
-
+height: 100%;
+  flex: 1;
 `
 const InfoContainer = styled.div`
+flex: 1;
+padding: 50px;
+`
+const Title = styled.h1`
+font-size: 70px;
+`
 
+const Desc = styled.p`
+margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`
+
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+  border-radius: 20%;
 `
 
 const Slider = () => {
@@ -48,13 +79,36 @@ const Slider = () => {
             <ArrowBackIosNewIcon></ArrowBackIosNewIcon>
         </Arrow>
         <Wrapper>
+           <Slide> 
             <ImgContainer>
-               
+               <Image src = "https://i.ibb.co/DG69bQ4/2.png"></Image>
             </ImgContainer>
             <InfoContainer>
-                
+                <Title>Sumer Sale</Title>
+                <Desc>ASDsadasf SADSADsafa fds fdss gfdgdfdsf  d fsf sf  dsf</Desc>
+                <Button>Show Now</Button>
             </InfoContainer>
-        </Wrapper>
+            </Slide>
+            <Slide> 
+            <ImgContainer>
+               <Image src = "https://i.ibb.co/DG69bQ4/2.png"></Image>
+            </ImgContainer>
+            <InfoContainer>
+                <Title>Sumer Sale</Title>
+                <Desc>ASDsadasf SADSADsafa fds fdss gfdgdfdsf  d fsf sf  dsf</Desc>
+                <Button>Show Now</Button>
+            </InfoContainer>
+            </Slide>
+        </Wrapper><Slide> 
+            <ImgContainer>
+               <Image src = "https://i.ibb.co/DG69bQ4/2.png"></Image>
+            </ImgContainer>
+            <InfoContainer>
+                <Title>Sumer Sale</Title>
+                <Desc>ASDsadasf SADSADsafa fds fdss gfdgdfdsf  d fsf sf  dsf</Desc>
+                <Button>Show Now</Button>
+            </InfoContainer>
+            </Slide>
         <Arrow direction = "right">
             <ArrowForwardIosIcon></ArrowForwardIosIcon>   
         </Arrow>
