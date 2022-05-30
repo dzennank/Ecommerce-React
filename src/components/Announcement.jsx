f61d8f6e545726e08bfb5fc2803e0ddc98835149
dzennank <dzenankrlic@gmail.com>
2022-05-30 15:42:29 +0200

    ..

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 
 const Container = styled.div`
 height: 30px;
-background-color: gray;
+  background-color: #5F9EA0;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+
 `
 
 const Announcement = () => {
