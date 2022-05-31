5b6ee81ccca3ed443c7cdfb772567ee89538867b
dzennank <dzenankrlic@gmail.com>
2022-05-31 13:29:06 +0200

    add categories and categoryItem components

---
+++ src/components/Categories.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components';
+import { categories } from '../data';
+import CategoryItem from './CategoryItem';
+
+const Container = styled.div`
+
+`
+
+const Categories = () => {
+  return (
+    <Container>
+        {categories.map((item) => (
+            <CategoryItem item = {item}/>
+        ))}
+    </Container>
+  )
+}
+
+export default Categories
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -33,7 +33,9 @@ const Arrow = styled.div`
 const Wrapper = styled.div`
 height: 100%;
   display: flex;
+  transition: all 1.5 ease;
   transform: translateX(${(props) => props.slideIndex * -100}vw);
+  
 `
 const Slide = styled.div`
   width: 100vw;
