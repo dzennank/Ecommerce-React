a5b2b2f12875f35ceeca5f2ddddd0d2926f13cf2
dzennank <dzenankrlic@gmail.com>
2022-06-01 14:39:14 +0200

    Product UI

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,8 +4,10 @@ import Product from './Product';
 import { popularProducts } from '../data';
 
 const Container = styled.div`
-padding: 20px;
-display: flex;
+     padding: 20px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
 `
 const Products = () => {
   return (
