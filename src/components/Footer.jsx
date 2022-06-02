d2bc8f6aede1b854e0836f2b92033f37270e6192
dzennank <dzenankrlic@gmail.com>
2022-06-02 13:21:32 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import ContactPhoneOutlinedIcon from '@mui/icons-material/ContactPhoneOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 
 const Container = styled.div`
     
@@ -31,6 +33,7 @@ flex: 1;
 padding: 20px;
 `
 const Logo = styled.h1`
+    margin-bottom: 30px;
 
 `
 const Desc = styled.p`
@@ -49,9 +52,12 @@ const SocialIcon = styled.div`
         align-items: center;
         justify-content: center;
         margin-right: 20px;
+        margin-top: 30px;
+
     
 `
 const Title = styled.h3`
+    margin-bottom: 30px;
 
 `
 const List = styled.ul`
@@ -66,47 +72,86 @@ width: 50%;
   margin-bottom: 10px;
 `
 
+const ContactInfo = styled.div`
+  padding: 20px;
+`
+const Icon = styled.div`
+
+`
+const Phone = styled.p`
+  margin-left: 20px;
+  font-size: 20px;
+`
+const Email = styled.p`
+  margin-left: 20px;
+  font-size: 20px;
+`
 
 
 const Footer = () => {
   return (
     <Container>
-    <Wrapper>
-        <Left>
-            <Logo>LOGO</Logo>
-            <Desc>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Desc>
-             <SocialContainer>
-                 <SocialIcon color="3B5999">
-                    <FacebookIcon></FacebookIcon>
-                 </SocialIcon>
-                 <SocialIcon color="E4405F">
-                    <InstagramIcon></InstagramIcon>
-                 </SocialIcon>
-                 <SocialIcon color="0000FF">
-                    <LinkedInIcon></LinkedInIcon>
-                 </SocialIcon>
-             </SocialContainer>
-        </Left>
-        <Center>
-        <Title>Useful Links</Title>
-        <List>
-          <ListItem>Home</ListItem>
-          <ListItem>Cart</ListItem>
-          <ListItem>Man Fashion</ListItem>
-          <ListItem>Woman Fashion</ListItem>
-          <ListItem>Accessories</ListItem>
-          <ListItem>My Account</ListItem>
-          <ListItem>Order Tracking</ListItem>
-          <ListItem>Wishlist</ListItem>
-          <ListItem>Wishlist</ListItem>
-          <ListItem>Terms</ListItem>
-        </List>
-        </Center>
-        <Right></Right>
-        </Wrapper>
+      <Wrapper>
+        
+          <Left>
+              
+              <Logo>LOGO</Logo>
+              
+              <Desc>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Desc>
+              
+              <SocialContainer>
+                  <SocialIcon color="3B5999">
+                      <FacebookIcon></FacebookIcon>
+                  </SocialIcon>
+                  <SocialIcon color="E4405F">
+                      <InstagramIcon></InstagramIcon>
+                  </SocialIcon>
+                  <SocialIcon color="0000FF">
+                      <LinkedInIcon></LinkedInIcon>
+                  </SocialIcon>
+              </SocialContainer>
+
+          </Left>
+          
+          <Center>
+          
+          <Title>Useful Links</Title>
+          
+          <List>
+            <ListItem>Home</ListItem>
+            <ListItem>Cart</ListItem>
+            <ListItem>Man Fashion</ListItem>
+            <ListItem>Woman Fashion</ListItem>
+            <ListItem>Accessories</ListItem>
+            <ListItem>My Account</ListItem>
+            <ListItem>Order Tracking</ListItem>
+            <ListItem>Wishlist</ListItem>
+            <ListItem>Wishlist</ListItem>
+            <ListItem>Terms</ListItem>
+          </List>
+          
+          </Center>
+          
+          <Right>
+            <Title>Contact</Title>
+            <ContactInfo>
+              <Icon>
+              <ContactPhoneOutlinedIcon></ContactPhoneOutlinedIcon>
+              <Phone>0612665680</Phone>
+              </Icon>
+              <Icon>
+                <EmailOutlinedIcon></EmailOutlinedIcon>
+                <Email>dzenankrlic@gmail.com</Email>
+              </Icon>
+
+              
+
+            </ContactInfo>
+          </Right>
+      </Wrapper>
     </Container>
   )
 }
