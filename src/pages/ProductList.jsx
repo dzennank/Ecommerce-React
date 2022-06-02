319ce023d42283e3785fc79b0a663ed1e0755e0e
dzennank <dzenankrlic@gmail.com>
2022-06-02 14:18:01 +0200

    filter

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -19,6 +19,18 @@ const FilterContainer = styled.div`
 const Filter = styled.div`
     margin: 20px;
 `
+const FilterText = styled.span`
+    font-size: 20px;
+    font-weight: 600;
+    margin-right: 20px;
+`
+const Select = styled.select`
+padding: 10px;
+  margin-right: 20px;
+`
+const Option = styled.option`
+
+`
 
 const ProductList = () => {
   return (
@@ -27,8 +39,39 @@ const ProductList = () => {
         <Announcement />
         <Title>Dresses</Title>
         <FilterContainer>
-            <Filter>Filter 1</Filter>
-            <Filter>Filter 2</Filter>
+            <Filter>
+                <FilterText>Filter Products:</FilterText>
+                <Select>
+            <Option disabled selected>
+              Color
+            </Option>
+            <Option>White</Option>
+            <Option>Black</Option>
+            <Option>Red</Option>
+            <Option>Blue</Option>
+            <Option>Yellow</Option>
+            <Option>Green</Option>
+          </Select>
+          <Select>
+            <Option disabled selected>
+              Size
+            </Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+          </Select>
+            </Filter>
+           
+            <Filter>
+                <FilterText>Sort Products:</FilterText>
+                <Select>
+            <Option selected>Newest</Option>
+            <Option>Price (asc)</Option>
+            <Option>Price (desc)</Option>
+          </Select>
+            </Filter>
         </FilterContainer>
         <Products />
         <Newsletter />
