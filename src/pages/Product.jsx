78e730f2f02e0cd6ddb8bbed5c7451c4f2e06ab9
dzennank <dzenankrlic@gmail.com>
2022-06-02 15:29:04 +0200

    prduct page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -4,6 +4,8 @@ import Announcement from '../components/Announcement'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import Newsletter from '../components/Newsletter'
+import { Add, Remove } from '@mui/icons-material';
+import { Button } from '@mui/material';
 
 const Container = styled.div`
 
@@ -23,6 +25,7 @@ const Image = styled.img`
 `
 const InfoContainer = styled.div`
  flex: 1;
+ margin-left: 20px;
 `
 const Title = styled.h1`
     font-weight: 200;
@@ -34,6 +37,57 @@ const Price = styled.span`
   font-weight: 100;
   font-size: 40px;
 `;
+
+const FilterContainer = styled.div`
+    width: 50%;
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
+`
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`
+const FilterSizeOption = styled.option``
+
+const AddContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 50%;
+`
+const AmountContainer = styled.div`
+    display: flex;
+  align-items: center;
+  font-weight: 700;
+`
+const Amount = styled.p`
+    width: 30px;
+    height: 30px;
+    border-radius: 10px;
+    border: 1px solid teal;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0px 5px;
+`
+
 
 const Product = () => {
   return (
@@ -52,6 +106,34 @@ const Product = () => {
             tristique tortor pretium ut. Curabitur elit justo, consequat id
             condimentum ac, volutpat ornare.</Desc>
             <Price>$ 20</Price>
+            <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+
+            <AddContainer>
+                <AmountContainer>
+                    <Remove />
+                    <Amount>1</Amount>
+                    <Add />
+                </AmountContainer>
+                <Button>ADD to Cart</Button>
+            </AddContainer>
+
             </InfoContainer>
         </Wrapper>
         <Newsletter />
