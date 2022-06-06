573ff8fd8e0a1ff7876132b6071d245b1a93c3f2
dzennank <dzenankrlic@gmail.com>
2022-06-06 14:10:36 +0200

    register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,30 +8,52 @@ const Container = styled.div`
         rgba(255, 255, 255, 0.5),
         rgba(255, 255, 255, 0.5)
         ),
-        url("https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+        url("")
         center;
     background-size: cover;
     display: flex;
     align-items: center;
     justify-content: center;
-`
-const Wrapper = styled.div`
+
     
 `
+const Wrapper = styled.div`
+    width: 40%;
+    padding: 20px;
+    background-color: lightgray;
+    display: flex;
+    align-items: center;
+    
+    `
 const Title = styled.h1`
-
+    font-size: 24px;
+    font-weight: 300;
+    margin-right: 20px;
 `
 const Form = styled.form`
-
+    display: flex;
+    flex-wrap: wrap
 `
 const Input = styled.input`
-
+    flex: 1;
+    min-width: 40%;
+    margin: 20px 10px 0px 0px;
+    padding: 10px;
 `
 const Agreement = styled.div`
-
+    font-size: 12px;
+    
 `
 const Button = styled.button`
-
+    width: 40%;
+    border: none;
+    border-radius: 20%;
+    padding: 15px 20px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+    margin-left: 20px;
+    
 `
 
 
