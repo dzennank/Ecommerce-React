735648cb10f660c835d5ad76a396c038cd836029
dzennank <dzenankrlic@gmail.com>
2022-06-07 15:40:28 +0200

    car page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,3 +1,4 @@
+import { Add, Remove } from '@mui/icons-material';
 import React from 'react'
 import styled from 'styled-components';
 import Announcement from '../components/Announcement'
@@ -83,10 +84,57 @@ const ProductColor = styled.div`
   border-radius: 50%;
   background-color: ${(props) => props.color}
 `
+const PrizeDetail = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+const ProdcutAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`
+const ProductAmount = styled.span`
+  font-size: 24px;
+  margin: 5px;
+`
+const ProductPrize = styled.span`
+  font-size: 30px;
+  font-weight: 200;
+`
+
 
 const Summary = styled.div`
-flex: 1;
+    flex: 1;
+    border: 0.5px solid lightgray;
+    border-radius: 10px;
+    padding: 20px;
+    height: 50vh;
 `
+const SummaryTitle = styled.h1`
+  font-weight: 200;
+`
+const SummaryItem = styled.div`
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === "total" && "500"};
+  font-size: ${(props) => props.type === "total" && "24px"};
+`;
+
+const SummaryItemText = styled.span``;
+
+const SummaryItemPrice = styled.span``;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: black;
+  color: white;
+  font-weight: 600;
+`;
 
 const Cart = () => {
   return (
@@ -121,9 +169,36 @@ const Cart = () => {
                   </ProductSize>
                 </Details>
               </ProductDetail>
+              <PrizeDetail>
+                <ProdcutAmountContainer>
+                  <Add />
+                  <ProductAmount>2</ProductAmount>
+                  <Remove />
+                </ProdcutAmountContainer>
+                <ProductPrize>60$</ProductPrize>
+              </PrizeDetail>
             </Product>
           </Info>
-          <Summary>Summary</Summary>
+          <Summary>
+          <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal</SummaryItemText>
+              <SummaryItemPrice>$ 80</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Estimated Shipping</SummaryItemText>
+              <SummaryItemPrice>$ 5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping Discount</SummaryItemText>
+              <SummaryItemPrice>$ -5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemText>Total</SummaryItemText>
+              <SummaryItemPrice>$ 80</SummaryItemPrice>
+            </SummaryItem>
+            <Button>CHECKOUT NOW</Button>
+          </Summary>
         </Bottom>
       </Wrapper>
       <Footer />
